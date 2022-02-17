a51ee09e0fdb9f5af303ebe75bd043a57f96d69c
KevinMejia <kevinmejia_2010@hotmail.com>
2022-02-16 22:06:30 -0600

    Se agregaron funciones para realizar los calculos

--- figuras.js
+++ figuras.js
@@ -1,47 +1,15 @@
 //Código de cuadrado
-console.group("Cuadrados")
-const ladoCuadrado = 5;
-console.log(`Los lados del cuadrado miden: ${ladoCuadrado} unidades`);
 
-const perimetro = ladoCuadrado * 4;
-console.log(`El perimetro del cuadrado es: ${perimetro} unidades`);
+const perimetroCuadrado = (lado)=> lado * 4;
+const area = (lado) => lado * lado;
 
-const area = ladoCuadrado * ladoCuadrado;
-console.log(`El área del cuadrado es: ${area} unidades cuadradas`);
-
-console.groupEnd()
 //Código del triángulo
 
-console.group("Triángulo")
-const lado1T = 6;
-const lado2T = 6;
-const baseT = 4;
-const alturaT = 5.5;
-console.log(`Los lados del triángulo son: \n lado1: ${lado1T} unidades \n lado2: ${lado2T} unidades \n base: ${baseT} unidades `);
-
-const perimetroTriangulo = lado1T + lado2T + baseT;
-console.log(`El perimetro del triángulo son: ${perimetroTriangulo} unidades`);
-
-const areaT = (baseT * alturaT)/2;
-console.log(`El área del triángulo es: ${areaT} unidades cuadradas`);
-
-console.groupEnd();
+const perimetroTriangulo = (lado1, lado2, base) => lado1 + lado2 + base;
+const areaTriangulo = (base, altura) => (base * alturaT)/2;
 
 //Código del circulo
 
-console.group("Circulo")
-
-const radio = 3;
-const diametro = 2 * radio;
-const pi = 3.1416
-
-console.log(`El radio del circulo es: radio: ${radio} unidades `);
-console.log(`El diámetro del circulo es: radio: ${diametro} unidades `);
-
-const circunferencia = 2*radio*pi;
-console.log(`El circunferencia del circulo son: ${perimetroTriangulo} unidades`);
-
-const areaC = 2 * (radio * radio * pi);
-console.log(`El área del circulo es: ${areaC} unidades cuadradas`);
-
-console.groupEnd();
+const pi = Math.PI;
+const circunferencia = (radio) => radio * 2 * pi;
+const areaCirculo = (radio) => radio * radio * pi;
